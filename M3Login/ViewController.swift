ab7c6946ec35a66cba94cb8bb77df31079951865
minesettimi <minesettimi@gmail.com>
2026-02-03 11:54:42 -0500

    initial login

--- M3Login/ViewController.swift
+++ M3Login/ViewController.swift
@@ -9,6 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var idInput: UITextField!
+    @IBOutlet weak var passInput: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func pressBtn(_ sender: UIButton) {
+        if (idInput.text == "NiccoS" && passInput.text == "1234")
+        {
+            resultLabel.text = "Login Successful"
+        }
+        else
+        {
+            resultLabel.text = "Login Failed"
+        }
+        
+        idInput.text = ""
+        passInput.text = ""
+    
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
