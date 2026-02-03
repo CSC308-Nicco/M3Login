a6d92a048742196f232d1c85993bf22b164931a1
minesettimi <minesettimi@gmail.com>
2026-02-03 11:10:40 -0500

    Initial Commit

---
+++ M3Login/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M3Login
+//
+//  Created by Settimi, Niccolo B. on 2/3/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
